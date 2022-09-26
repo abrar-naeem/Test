4b4a440b0497f72b7a02591085c58deec6bc3e70
abrar-naeem <114279275+abrar-naeem@users.noreply.github.com>
2022-09-26 14:50:58 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,20 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("ok");
+		
+		System.out.println("ll");
+		System.out.println("llkk");
+		System.out.println("llkm");
+
+		
+
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
